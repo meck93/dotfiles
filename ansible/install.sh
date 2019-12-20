a708fe76894d49e9c313a8e9f12d9c53450964fb
Moritz Eck <moritz.eck@gmail.com>
2019-12-20 11:31:18 +0100

    Update install.sh
    wrong readme entry

--- ansible/install.sh
+++ ansible/install.sh
@@ -19,5 +19,5 @@ fi
 # Display real installation process #
 echo ""
 echo "Customize the playbook ansible-desktop.yml to suit your needs, then run ansible with :"
-echo "  ansible-playbook ansible-desktop.yml --ask-become-pass"
+echo "  ansible-playbook local.yml --ask-become-pass"
 echo ""
